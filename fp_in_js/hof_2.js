8338464ce92a6dd31be34832bfc47128fe47e115
robcrockPI <robert@propellerindustries.com>
2020-08-11 07:25:25 -0400

    Use closure to finish these exercises.

--- fp_in_js/hof_2.js
+++ fp_in_js/hof_2.js
@@ -1,17 +1,35 @@
 weapons_with_noises = [
-    	{name: 'Phaser', noise: 'bssszzsssss', universe: 'Star Trek'},
-	{name: 'Blaster', noise: 'Pew Pew', universe: 'Star Wars'},
-	{name: 'Sonic Screwdriver', noise: 'Pew Pew', universe: 'Dr. Who'},
-	{name: 'Lightsaber', noise: 'Pew Pew', universe: 'Star Wars'},
-	{name: 'Noisy Cricket', noise: 'Pew Pew', universe: 'Men in Black'}
-]
+  { name: 'Phaser', noise: 'bssszzsssss', universe: 'Star Trek' },
+  { name: 'Blaster', noise: 'Pew Pew', universe: 'Star Wars' },
+  { name: 'Sonic Screwdriver', noise: 'Pew Pew', universe: 'Dr. Who' },
+  { name: 'Lightsaber', noise: 'Pew Pew', universe: 'Star Wars' },
+  { name: 'Noisy Cricket', noise: 'Pew Pew', universe: 'Men in Black' },
+];
 
 function weaponsFromUniverse(universe) {
-	// ...Your code here!
+  // ...Your code here!
+  const universeWeapons = weapons_with_noises.filter(
+    weapon => weapon.universe === universe
+  );
+
+  return function(weaponParam) {
+    const chosenWeapon = universeWeapons.filter(
+      weapon => weapon.name === weaponParam
+    );
+
+    if (chosenWeapon.length === 0) {
+      return `${weaponParam}is not a part of the ${universe} universe`;
+    }
+    return `used ${weaponParam}: ${chosenWeapon[0].noise}!`;
+  };
 }
 
 // USAGE
-const useStarWarsWeapon = weaponsFromUniverse('Star Wars')
+// Closure around the universe weapons
+const useStarWarsWeapon = weaponsFromUniverse('Star Wars');
 
-useStarWarsWeapon('Blaster') // console logs 'used Blaster: Pew Pew'
-useStarWarsWeapon('Noisy Cricket') // console logs 'Noisy Cricket is not a part of the Star Wars universe'
+// return the appropriate weapon from the universe
+console.log(useStarWarsWeapon('Blaster'));
+
+// console logs 'Noisy Cricket is not a part of the Star Wars universe'
+console.log(useStarWarsWeapon('Noisy Cricket'));
