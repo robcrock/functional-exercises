e058961589d20a7b96b204a5c88c3fa301da64ad
robcrockPI <robert@propellerindustries.com>
2020-08-13 07:11:54 -0400

    Functional DOM manipulation exercise complete.

--- fp_in_js/render_method.js
+++ fp_in_js/render_method.js
@@ -1,35 +1,44 @@
+/* eslint-disable no-lone-blocks */
 // Here is the code from the example text
 
-const root = document.getElementById('root')
+const root = document.getElementById('root');
 
-const Welcome = () => {
-    return `Welcome to my Javascript Program!`
-}
-
-const App = () => {
-    return `
-        <h1>${Welcome()}<h1>
-        <div> I EXIST! </div>
-    `
-}
-
-const render = root => {
-    root.innerHTML = App()
-}
-
-render(root)
+const Welcome = () => `Welcome to my Javascript Program!`;
 
 // Add a new Menu component that takes in a show argument which is either true or false
 // Show this content if show is true:
-{/* <nav>
-    <ul>
-        <li>About Us</li>
-        <li>Contact Us</li>
-        <li>Login</li>
-    </ul>
-</nav> */}
+// {
+// }
 
 // and this content if show is false:
-{/* <nav>Menu</nav> */ }
+// {
+//   /* <nav>Menu</nav> */
+// }
 
 // Your Code Here
+const menu = show => {
+  const showIfTrue = `<nav>
+        <ul>
+            <li>About Us</li>
+            <li>Contact Us</li>
+            <li>Login</li>
+        </ul>
+    </nav>`;
+  const showIfFalse = `<nav>Menu</nav>`;
+  if (show) {
+    return showIfTrue;
+  }
+  return showIfFalse;
+};
+
+const App = () => `
+        <h1>${Welcome()}<h1>
+        ${menu(true)}
+        <div> I EXIST! </div>
+    `;
+
+const render = root => {
+  root.innerHTML = App();
+};
+
+console.log(render(root));
