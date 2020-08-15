b8a9e62ab64b27c3c59c64da902bea5fedbd693c
robcrockPI <robert@propellerindustries.com>
2020-08-15 17:08:55 -0400

    Work with Immutable js.

--- fp_in_js/application_state.js
+++ fp_in_js/application_state.js
@@ -1,34 +1,48 @@
 // Below is the code from the lesson
 
 let store = {
-    user: {
-        first_name: 'John',
-        last_name: 'Doe'
-    }
-}
-
-const render = (root, state) => {
-    root.innerHTML = App(state)
-}
-
-const Welcome = (name) => {
-    return `Welcome, ${name} to my Javascript Program!`
-}
-
-const App = (state) => {
-    return `
-       <h1>${Welcome(state.user.first_name)}<h1>
-      <div> I EXIST! </div>
-   `
-}
-
-window.addEventListener('load', () => {
-    render(root, store)
-})
+  user: {
+    first_name: 'John',
+    last_name: 'Doe',
+  },
+};
 
 // But what if state changes at some point in our app?
-// Using the object methods we have learned so far in this course, 
+// Using the object methods we have learned so far in this course,
 // create a function called updateStore that takes in the old state, the new state, and updates the old state with any new information
 // This won't be a pure function, instead of a return, call the render method again
 
 // Your Code
+const newStore = {
+  user: {
+    first_name: 'Robert',
+    last_name: 'Crocker',
+  },
+};
+
+let newState = Object.assign(store, newStore);
+console.log(newState);
+
+const render = (root, state) => {
+  root.innerHTML = App(state);
+};
+
+const Welcome = name => {
+  return `Welcome, ${name} to my Javascript Program!`;
+};
+
+const App = state => {
+  return `
+       <h1>${Welcome(state.user.first_name)}<h1>
+      <div> I EXIST! </div>
+   `;
+};
+
+window.addEventListener('load', () => {
+  render(root, store);
+});
+
+const updateStore = (store, newState) => {
+  store = Object.assign(store, newState);
+  render(root, store);
+};
--- further_with_funcational_js/immutablejs.js
+++ further_with_funcational_js/immutablejs.js
@@ -1,43 +1,51 @@
+/* eslint-disable no-undef */
 //immutablejs.js
 // PART 1. Getting and Setting values
 // Given the following object, make an update that turns name to 'Mal' and role to 'Captain'
 
-const state1 = Immutable.Map(const map1 = Immutable.Map({ 
+const state1 = Immutable.Map(
+  (map1 = Immutable.Map({
     name: 'Wash',
     ship: {
-        name: 'Serenity',
-        class: 'Firefly'
+      name: 'Serenity',
+      class: 'Firefly',
     },
     role: 'Pilot',
     favorite_thing: {
-        item: "Toy",
-        details: {
-            type: 'Toy Tyrannosaurus Rex'
-        }
-    }
-});
+      item: 'Toy',
+      details: {
+        type: 'Toy Tyrannosaurus Rex',
+      },
+    },
+  }))
+);
 
 // Your Code
+// const state2 = state1.set('name', 'Mal');
+// console.log(state2);
 
 // -----------------------------------------------------------------
 // -----------------------------------------------------------------
 
 // PART 2. More with objects
 // This is a normal javascript object. It is very different from an Immutable Map
-const obj = { d: 100, o: 200, g: 300 };
+// const obj = { d: 100, o: 200, g: 300 };
 
 // But thankfully, normal javascript objects are still valid in Immutable
 // Notice that here we can merge a normal object into an Immutable Map
-const map3 = state1.merge(state2, obj);
+// const map3 = state1.merge(state2, obj);
 
-// But also notice how different the two are when we console log map3 
-console.log(map3)
+// But also notice how different the two are when we console log map3
+// console.log(obj);
+// console.log(map3);
 
 // YOUR TURN --------------------------------------------------------------
 // Can you locate the contents of our variable 'obj' inside the Immutable Map map3?
-console.log(map3)
 
 //Your code
+// const objKeys = Object.keys(obj);
+// const map4 = map3.filter((v, i) => objKeys.includes(i));
+// console.log(map4);
 
 // -----------------------------------------------------------------
 // -----------------------------------------------------------------
@@ -45,24 +53,24 @@ console.log(map3)
 // PART 3. Arrays and Immutable Lists
 
 // An immutable array is called a LIST, and is declared like this:
-const numbers = List([1, 2, 3]);
+// const numbers = List([1, 2, 3]);
 
 // We can turn normal JS arrays into Immutable Lists like this:
-const plainArray = [ 1, 2, 3, 4 ]
-const listFromPlainArray = List(plainArray)
-console.log(listFromPlainArray)
+const plainArray = [1, 2, 3, 4];
+const listFromPlainArray = List(plainArray);
+console.log(listFromPlainArray);
 
 // we can declare a new Immutable List
-const myList = List([ 'stuffed t-rex' ]);
-console.log(Array.from(myList));
+// const myList = List(['stuffed t-rex']);
+// console.log(Array.from(myList));
 
 // and we can use the set method again to add or update values. Just provide the index first and value second
-const myList1 = myList.set(1, 'toy lightsaber');
-console.log(Array.from(myList1)); // [ "stuffed t-rex", "toy lightsaber" ]
+// const myList1 = myList.set(1, 'toy lightsaber');
+// console.log(Array.from(myList1)); // [ "stuffed t-rex", "toy lightsaber" ]
 
 // but, in order to not need to supply the index, we can also add new values with push
-const myList2 = myList1.push('Picard bobblehead')
-console.log(Array.from(myList2));
+// const myList2 = myList1.push('Picard bobblehead');
+// console.log(Array.from(myList2));
 
 // YOUR TURN --------------------------------------------------------------------------
 
@@ -74,7 +82,6 @@ console.log(Array.from(myList2));
 // -----------------------------------------------------------------
 // -----------------------------------------------------------------
 
-
 // STRETCH CHALLENGE ----------------------------------------------------------------------
 // 3. Remove Jabba the Hut from the Immutable List
 // look up in the documentation how you would remove a character from the Immutable list
@@ -85,34 +92,34 @@ const characters = [
     role: 'First Mate',
     universe: 'Hitchhikers Guide to the Galaxy',
     weapon: 'severe depression',
-    powerLevel: 1000
+    powerLevel: 1000,
   },
   {
     name: 'Jabba the Hut',
     role: 'villain',
     universe: 'Star Wars',
     weapon: 'henchmen',
-    powerLevel:  200
+    powerLevel: 200,
   },
   {
     name: 'Zoë Alleyne Washburne',
     role: 'First Mate',
     universe: 'Firefly',
     weapon: 'Winchester Model 1892 rifle',
-    powerLevel: 160
+    powerLevel: 160,
   },
   {
     name: 'Peter Venkman',
     role: 'Ghostbuster',
     universe: 'Ghostbusters',
     weapon: 'proton pack',
-    powerLevel: 120
+    powerLevel: 120,
   },
   {
     name: 'Kathryn Janeway',
     role: 'Captain',
     universe: 'Star Trek',
     weapon: 'Wit',
-    power_level: 140
-  }
-]
+    power_level: 140,
+  },
+];
